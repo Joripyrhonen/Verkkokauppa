c3ab4c4ec15e068d27df492e7aaccf98fa72bc49
Joripyrhonen <joripyrhonen@hotmail.com>
2024-12-09 10:40:18 +0200

    Ensimmäinen Commit kokeilu

--- Verkkokauppa/src/application/Main.java
+++ Verkkokauppa/src/application/Main.java
@@ -1,21 +1,19 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,Board.getWidth(),Board.getHeight());
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Visual.fxml"));
+			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setTitle("Chess");
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
---
+++ Verkkokauppa/src/application/Purchase.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class Purchase {
+	private ArrayList<Item> items;
+	private LocalDateTime purchaseDate;
+	public ArrayList<Item> getItems() {
+		return items;
+	}
+	public void setItems(ArrayList<Item> items) {
+		this.items = items;
+	}
+	public LocalDateTime getPurchaseDate() {
+		return purchaseDate;
+	}
+	public void setPurchaseDate(LocalDateTime purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+}
